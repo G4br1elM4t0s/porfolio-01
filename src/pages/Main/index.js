524383ec13cfe87440a35934199a111cef6fa267
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-01-25 12:28:48 -0300

    adicionando a funcionalidade de deletar um repo!

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,48 +1,53 @@
 import React, { useState, useCallback } from "react";
-import { FaGithub, FaPlus } from "react-icons/fa";
-import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus, FaSpinner,FaBars,FaTrash } from "react-icons/fa";
+import { Container, Form, SubmitButton,List,DeleteButton } from "./styles";
+
 
 import api from "../../api";
 
 export default function Main() {
   const [newRepo, setNewRepo] = useState("");
   const [repositorios, setRepositorios] = useState([]);
-  const [loading,setLoading] = useState(false);
-
+  const [loading, setLoading] = useState(false);
+  
   function handleinputChange(e) {
     setNewRepo(e.target.value);
   }
 
+/*nao esta sendo utilizado o async await pois estou utilizando os estados abaixo
+* entao fica bem mais objetivo, rapido e intuitivo usar o useCallback
+*/
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      setLoading(true);
+      async function submit() {
+        try {
+          const response = await api.get(`/repos/${newRepo}`); // buscando repos aula-05
+          const data = {
+            name: response.data.full_name,
+          };
 
-  /*nao esta sendo utilizado o async await pois estou utilizando os estados abaixo
-  * entao fica bem mais objetivo, rapido e intuitivo usar o useCallback 
-  */
-
-  const handleSubmit = useCallback((e) => {
-
-    e.preventDefault();
-    
-    async function submit() {
-      try{
-      const response = await api.get(`/repos/${newRepo}`); // buscando repos aula-05
-      const data = {
-        name: response.data.full_name
-      };
-
-      setRepositorios([...repositorios, data]);
-      setNewRepo("");
-      console.log(data.name)
-      }catch(error){
-        
-        console.log(error);
-      }finally{
-        setLoading(false);
+          setRepositorios([...repositorios, data]);
+          setNewRepo("");
+          console.log(data.name);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
       }
-      
-    }
 
-    submit();
-  }, [newRepo,repositorios]);
+      submit();
+    },
+    [newRepo, repositorios]
+  );
+
+  const handleDelete = useCallback((repo)=>{
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  },[repositorios])
+
 
   return (
     <Container>
@@ -59,10 +64,30 @@ export default function Main() {
           onChange={handleinputChange}
         />
 
-        <SubmitButton Loading={loading ? 1 : 0 }> 
-          <FaPlus color="#FFF" size={14} />
+        <SubmitButton  loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#FFF" size={14} />
+          )}
         </SubmitButton>
       </Form>
+
+      <List>
+          {repositorios.map(repo=>(
+            <li key={repo.name}>
+              <span>
+                <DeleteButton onClick={()=>{handleDelete(repo.name)}}>
+                 <FaTrash size={14}/>
+                </DeleteButton>
+                {repo.name}
+                </span>
+              <a href="#">
+              <FaBars size={20}/>
+              </a>
+            </li>
+          ))}
+      </List>
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {keyframes,css} from "styled-components";
 
 export const Container = styled.div`
   max-width: 700px;
@@ -19,7 +19,19 @@ export const Container = styled.div`
     }
 
   }
-`
+`;
+
+//criando animacao para o button
+
+const animate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+
+  to{
+    transform: rotate(360deg)
+  }
+`;
 
 export const SubmitButton = styled.button.attrs(props=>({
   type: "submit",
@@ -39,8 +51,16 @@ export const SubmitButton = styled.button.attrs(props=>({
   &[disabled]{
     cursor:not-allowed;
     opacity: 0.5;
-    background:red;
   }
+
+  ${props => props.loading  && 
+  css`
+    svg{
+      animation: ${animate} 2s linear infinite;
+    }
+  `
+  }
+
 `
 
 export const Form = styled.form`
@@ -54,4 +74,39 @@ export const Form = styled.form`
     padding: 8px;
     font-size:17px;
   }
+`
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 20px;
+
+  li{
+    padding: 15px 0;
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    
+    & + li{ // ignora a primeira li e so aplica as respectivas propiedades do segundo para baixo
+       border-top: 1px solid #EEE;
+    }
+
+    a{
+      color: #000;
+      text-decoration:none;
+    }
+  }
+
+`;
+
+export const DeleteButton = styled.button.attrs({
+  type:'button'
+})`
+  margin-left: 6px;
+  background: transparent;
+  color:#431259;
+  border: none;
+  padding: 8px 7px;
+  outline: 0;
+  border-radius: 4px;
 `
